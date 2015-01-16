a37599462fe61514da0f76cbcaadb114b75d053b
ayh <benayh@gmail.com>
2015-01-16 17:24:51 +0200

    new jdk 8 collections

--- src/com/my/Hello.java
+++ src/com/my/Hello.java
@@ -1,5 +1,8 @@
 package com.my;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.my.IEcho;
 
 public class Hello {
@@ -17,11 +20,17 @@ public class Hello {
 			
 		}).echo("Selam !");
 	
-		
-		
+				
 		IEcho j8 =  (s) -> System.out.println(">> " + s );
 		
 		j8.echo("I am java8");
+		
+		int[] arr = {1,2,3,4};
+		
+		List<String> list = Arrays.asList("1","2","3","4");
+	
+		list.forEach( s->System.out.println(s));
+				
 		
 	}
 
--- src/com/my/IEcho.java
+++ src/com/my/IEcho.java
@@ -1,3 +1,4 @@
+
 package com.my;
 
 public interface IEcho {
