cf6e6b34a1f227f6cf0098c8623fcf5a56b0d6b5
ayh <benayh@gmail.com>
2015-01-20 09:11:40 +0200

    Thread join & synchronize

---
+++ src/com/my/threads/JoinTest.java
@@ -0,0 +1,37 @@
+package com.my.threads;
+
+public class JoinTest {
+    
+	public int sharedCounter = 0;
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		JoinTest app = new JoinTest();
+		
+		Thread t1 = new Thread(()->app.doWork());
+		
+		Thread t2 = new Thread(()->app.doWork());
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+		
+		System.out.println(app.sharedCounter);
+
+	}
+	
+	
+	public void doWork() {
+		for (int i=0;i<100000;i++) 
+			 increment();
+	}
+	
+	public synchronized void increment() {
+		sharedCounter++;
+	}
+	
+}
+
+
